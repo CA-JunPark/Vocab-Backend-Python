9a0aa4b69851a7dcbbc2d37121757db37819db0d
CA-JunPark <cskoko5786@gmail.com>
2025-12-30 20:46:50 -0800

    basic Fast API

---
+++ WordSchema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class WordSchema(BaseModel):
+    name: str
+    meaningKr: str
+    example: str
+    antonymEn: str
+    tags: Optional[str] = None
+    createdTime: Optional[str] = None
+    modifiedTime: Optional[str] = None
+    isDeleted: bool = False
+    synced: bool = False
