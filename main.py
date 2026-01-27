15fb4cc83c1ef8c7eb1419a0926d12b95658de69
CA-JunPark <cskoko5786@gmail.com>
2026-01-26 23:19:14 -0800

    sync

--- main.py
+++ main.py
@@ -36,8 +36,6 @@ async def sync(request: SyncRequest):
     print("lastSyncTime: ", request.lastSyncTime)
     print("localChanges: ", request.localChanges)
     client = app.state.db_client
-    if not request.lastSyncTime:
-        return {"wordsToUpdate": []}
 
     for word in request.localChanges:
         await client.execute(
@@ -53,68 +51,37 @@ async def sync(request: SyncRequest):
                 isDeleted=excluded.isDeleted,
                 syncedTime=excluded.modifiedTime,
                 note=excluded.note
+            WHERE excluded.modifiedTime > Word.modifiedTime
             """,
             [word.name, word.meaningKr, word.example, word.antonymEn, word.tags, 
              word.createdTime, word.modifiedTime, int(word.isDeleted), word.modifiedTime, word.note]
         )
 
-    # Handle PULL: Get words updated by OTHER devices
-    result = await client.execute(
-        "SELECT * FROM Word WHERE modifiedTime > ? ORDER BY modifiedTime ASC", 
-        [request.lastSyncTime]
-    )
-
     updates = []
-    for row in result.rows:
-        updates.append({
-            "name": row[0],
-            "meaningKr": row[1],
-            "example": row[2],
-            "antonymEn": row[3],
-            "tags": row[4],
-            "createdTime": row[5],
-            "modifiedTime": row[6],
-            "isDeleted": bool(row[7]),
-            "syncedTime": row[8],
-            "note": row[9]
-        })
+    if request.lastSyncTime:
+        result = await client.execute(
+            "SELECT * FROM Word WHERE modifiedTime > ? ORDER BY modifiedTime ASC", 
+            [request.lastSyncTime]
+        )
 
-    print("wordsToUpdate: ", updates)
+        for row in result.rows:
+            updates.append({
+                "name": row[0],
+                "meaningKr": row[1],
+                "example": row[2],
+                "antonymEn": row[3],
+                "tags": row[4],
+                "createdTime": row[5],
+                "modifiedTime": row[6],
+                "isDeleted": bool(row[7]),
+                "syncedTime": row[8],
+                "note": row[9]
+            })
+    print("wordsToUpdate: ", len(updates))
     return {
         "wordsToUpdate": updates,
-        "serverTime": datetime.utcnow().isoformat() # Send server time to avoid clock drift
+        "serverTime": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S') # for update lastSyncTime in local
     }
-
-@app.post("/sync/push")
-async def push_changes(tasks: List[WordSchema]):
-    client = app.state.db_client
-    
-    # batch insert
-    queries = []
-    for task in tasks:
-        queries.append((
-            """
-            INSERT INTO Word (name, meaningKr, example, antonymEn, tags, createdTime, modifiedTime, isDeleted, syncedTime, note) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            ON CONFLICT(name) DO UPDATE SET
-            meaningKr=excluded.meaningKr,
-            example=excluded.example,
-            antonymEn=excluded.antonymEn,
-            tags=excluded.tags,
-            modifiedTime=excluded.modifiedTime,
-            isDeleted=excluded.isDeleted,
-            syncedTime=excluded.syncedTime,
-            note=excluded.note
-            """,
-            [task.name, task.meaningKr, task.example, task.antonymEn, 
-             task.tags, task.createdTime, task.modifiedTime, task.isDeleted, task.syncedTime, task.note]
-        ))
-    
-    results = await client.batch(queries)
-    success = sum(res.rows_affected for res in results)
-
-    # get all updated [word.name] 
-    return {"message": "Success: " + str(success)}
 
 @app.get("/sync/pullAll")
 async def pull_changes():
